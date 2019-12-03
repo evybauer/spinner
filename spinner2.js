c72e801f9459e09b25575f2bf17278a5b9a21ee6
Evelyn Bauer <evybauer@gmail.com>
2019-12-03 23:22:07 +0000

    Refactored spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -2,7 +2,7 @@ const spinFunc = function(spinner) {
   for (const spin in spinner) {
     setTimeout(() => {
       process.stdout.write(spinner[spin]);
-    }, 100 + spin * 200);
+    },spin * 200);
   }
 };
 
